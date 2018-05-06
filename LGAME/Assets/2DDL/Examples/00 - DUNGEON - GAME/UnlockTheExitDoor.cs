d32ee8faf72b3f67b9b99d45d44b016c83d3fa1b
Kevin <kevin.shi0705@gmail.com>
2018-05-06 15:37:53 +0800

    commit test2

---
+++ LGAME/Assets/2DDL/Examples/00 - DUNGEON - GAME/UnlockTheExitDoor.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnlockTheExitDoor : MonoBehaviour {
+	
+	// Blue torch
+	public  DynamicLight2D.DynamicLight light2d;
+
+	public GameObject traesure1;
+	public GameObject traesure2;
+	public GameObject traesure3;
+
+	// Internals
+	internal GameObject[] GOsReached;
+	
+	// privates
+	private bool isGateOpen = false;
+	private bool openning = false; 
+	private AudioSource openGateSound;
+		
+		
+		
+		
+	void Start () {
+		
+		// Add listener
+		if(light2d)
+			light2d.OnEnterFieldOfView += waveReach;
+
+		openGateSound = GetComponent<AudioSource>();
+		
+	}
+		
+		
+		
+	//- this function iterate on each object passed by 2DLigh script and compare if this object is the desired object Lit.
+	//-- THIS SCRIPT MUST BE ATTACHED TO PLAYER GO --//
+	
+	bool found1 = false;
+	bool found2 = false;
+	bool found3 = false;
+
+	void waveReach(GameObject g){//zxc
+				
+
+		//foreach(GameObject gs in g){
+			if(traesure1.GetHashCode() == g.GetHashCode()){
+				found1 = true;
+			}
+			if(traesure2.GetHashCode() == g.GetHashCode()){
+				found2 = true;
+			}
+			if(traesure3.GetHashCode() == g.GetHashCode()){
+				found3 = true;
+			}
+		//}
+		if(found1 && found2 && found3 && isGateOpen == false){
+			OpenThisGate();
+			Debug.Log("yeahh");
+		}
+	}
+
+	internal void OpenThisGate(){
+		// First unsubscribe event
+		light2d.OnEnterFieldOfView -= waveReach;
+
+		isGateOpen = true;
+
+		openning = true;
+
+		openGateSound.Play();
+
+	}
+
+	void Update(){
+		if(openning == true){
+			Vector3 p = transform.position;
+			p.y += 0.2f * Time.deltaTime;
+			transform.position = p;
+
+			Debug.Log("opening");
+
+			if(transform.localPosition.y >= -0.05f)
+				openning = false;
+		}
+	}
+
+}
---
+++ LGAME/Assets/2DDL/Examples/3 - Visibility and CoverExplosion(shockwave)/bomb.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bomb : MonoBehaviour {
+
+	DynamicLight2D.DynamicLight thisLight;
+	TimerClass timer;
+	GameObject player;
+
+	float tmpValue = 0;
+
+	Vector3 pos;
+
+
+	IEnumerator Start () {
+		thisLight = GameObject.Find("2DLight").GetComponent<DynamicLight2D.DynamicLight>();
+		player = GameObject.Find("MartinHead");
+
+		timer = gameObject.AddComponent<TimerClass>();
+
+
+		// Subscribe timer events //
+		timer.OnUpdateTimerEvent += timerUpdate;
+		timer.OnTargetTimerEvent += tick;
+
+		timer.InitTimer(1.2f, true);
+
+		StartCoroutine(LoopUpdate());
+	
+		yield return  null;
+
+	}
+
+
+		IEnumerator LoopUpdate(){
+
+		while(true){
+			pos.x += Input.GetAxis ("Horizontal") * 20f * Time.deltaTime;
+			pos.y += Input.GetAxis ("Vertical") * 20f * Time.deltaTime;
+			
+
+			Vector3 martinPos = Input.mousePosition;
+			martinPos = Camera.main.ScreenToWorldPoint(martinPos);
+			martinPos.z = 0;
+
+
+			yield return new WaitForEndOfFrame();
+			thisLight.gameObject.transform.position = pos;
+			player.transform.position = martinPos;
+		}
+
+		
+
+
+
+	}
+
+	
+	void tick(){
+		thisLight.LightRadius = 1.7f;
+		tmpValue *=0;
+	}
+	void timerUpdate(float value){
+	thisLight.LightRadius += value*.8f;
+	}
+}
